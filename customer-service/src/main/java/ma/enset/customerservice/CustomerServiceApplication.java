b13b8229bd7f5ac3129056d20b95e906b38c4ea5
hanane <hananeiouiri1@gmail.com>
2025-12-25 18:09:50 +0100

    Add Customer entity and repository

--- customer-service/src/main/java/ma/enset/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/ma/enset/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,38 @@
 package ma.enset.customerservice;
 
+import ma.enset.customerservice.entities.Customer;
+import ma.enset.customerservice.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner commandLineRunner(CustomerRepository customerRepository ){
+        return args -> {
+            customerRepository.save(Customer.builder()
+                    .name("Soukaina").email("Hanane@gmail.com")
+                    .build());
+            customerRepository.save(Customer.builder()
+                    .name("Ali").email("Ali@gmail.com")
+                    .build());
+            customerRepository.save(Customer.builder()
+                    .name("Imane").email("Imane@gmail.com")
+                    .build());
+            customerRepository.findAll().forEach(c->{
+                System.out.println("=======================");
+                System.out.println(c.getId());
+                System.out.println(c.getName());
+                System.out.println(c.getEmail());
+                System.out.println("=======================");
+            });
+        };
     }
 
 }
